93bc26e364d2c0107a976d751c0da409bf75c41a
AntoineBardy <antoine.bardy11@gmail.com>
2020-12-23 16:35:02 +0100

    Update RpgJS.js

--- RpgJS.js
+++ RpgJS.js
@@ -2,3 +2,8 @@ var MatthieuLeBoss =    {name:"MatthieuLeBoss", health:400, mana:100, attack:12,
 var MiniLaink =    {name:"MiniLaink", health:800, mana:40, attack:10, defense:10, lastAction:null};
 var MuscleTom =      {name:"MuscleTom",   health:500, mana:100, attack:8, defense:5, lastAction:null};
 var Bouclette =      {name:"Bouclette",   health:400, mana:70, attack:20, defense:3, lastAction:null};
+
+var pumpkin =   {element:null, class:"Monster", health:100, attack:8};
+var AlienCaptain =   {element:null, class:"Monster", health:200, attack:10};
+var SkullDemon =   {element:null, class:"Monster", health:400, attack:15};
+
