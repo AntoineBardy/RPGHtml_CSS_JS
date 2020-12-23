3a26e0864022bc2be0c284be5cb81f6d0e871835
AntoineBardy <antoine.bardy11@gmail.com>
2020-12-23 16:32:56 +0100

    Create RpgJS.js

---
+++ RpgJS.js
@@ -0,0 +1,4 @@
+var MatthieuLeBoss =    {name:"MatthieuLeBoss", health:400, mana:100, attack:12, defense:5 , lastAction:null};
+var MiniLaink =    {name:"MiniLaink", health:800, mana:40, attack:10, defense:10, lastAction:null};
+var MuscleTom =      {name:"MuscleTom",   health:500, mana:100, attack:8, defense:5, lastAction:null};
+var Bouclette =      {name:"Bouclette",   health:400, mana:70, attack:20, defense:3, lastAction:null};
